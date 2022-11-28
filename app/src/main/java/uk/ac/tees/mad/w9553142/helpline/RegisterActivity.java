48ee37635eb78cb6e69ef74c988d22c00d7ffead
Ushasriayiluri <ayiluriushasri@gmail.com>
2022-11-28 18:40:19 +0000

    Login and Register Activities are created

---
+++ app/src/main/java/uk/ac/tees/mad/w9553142/helpline/RegisterActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.mad.w9553142.helpline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
